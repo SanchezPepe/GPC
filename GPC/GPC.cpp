28724b431777d8c50b78ec9af8f76ff74a19ea97
SanchezPepe <sanchezpepe97@gmail.com>
2017-09-18 08:38:19 -0500

    Inicial

---
+++ GPC/GPC.cpp
@@ -0,0 +1,18 @@
+// GPC.cpp: define el punto de entrada de la aplicación de consola.
+// Preprocesador, está antes del copilador, donde se importan las librerías
+#include "stdafx.h"
+#include <iostream>
+#include <fstream>
+#include <strstream>
+using namespace std;
+
+int res = 3;
+int *puntero = &res;
+
+//int main()
+//{
+//	cout << *puntero << endl;
+//	cin.get();
+//    return 0;
+//}
+
